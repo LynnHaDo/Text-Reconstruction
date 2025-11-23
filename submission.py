90602aa62e64de8053b580bf27fb7b62988bbab3
Lynn Do <do24l@mtholyoke.edu>
2025-11-23 18:11:52 -0500

    Implement word segmentation with vowel insertion

--- submission.py
+++ submission.py
@@ -53,26 +53,39 @@ class VowelInsertionProblem(util.SearchProblem):
         self.possibleFills = possibleFills
 
     def startState(self):
-        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
-        # END_YOUR_CODE
+        return (0, wordsegUtil.SENTENCE_BEGIN)
 
     def isEnd(self, state) -> bool:
-        # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
-        # END_YOUR_CODE
+        index, _ = state 
+        return index == len(self.queryWords)
 
     def succAndCost(self, state):
-        # BEGIN_YOUR_CODE (our solution is 8 lines of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
-        # END_YOUR_CODE
+        successors = []
+        
+        index, previous_word = state 
+        current_word = self.queryWords[index]
+        possible_words = self.possibleFills(current_word)
+        
+        if possible_words is None:
+            possible_words.append(current_word)
+        
+        for word in possible_words:
+            successors.append((word, (index + 1, word), self.bigramCost(previous_word, word)))
+        
+        return successors
 
 
 def insertVowels(queryWords: List[str], bigramCost: Callable[[str, str], float],
         possibleFills: Callable[[str], Set[str]]) -> str:
-    # BEGIN_YOUR_CODE (our solution is 3 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+    if len(queryWords) == 0:
+        return ''
+
+    ucs = util.UniformCostSearch(verbose=0)
+    ucs.solve(VowelInsertionProblem(queryWords, bigramCost, possibleFills))
+
+    if ucs.actions is None:
+        return '' # cannot segment this query
+    return ' '.join(ucs.actions) # minimum-cost path is the segmented words with lowest unigram cost
 
 
 ############################################################
@@ -86,19 +99,28 @@ class JointSegmentationInsertionProblem(util.SearchProblem):
         self.possibleFills = possibleFills
 
     def startState(self):
-        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
-        # END_YOUR_CODE
+        return (0, wordsegUtil.SENTENCE_BEGIN)
 
     def isEnd(self, state) -> bool:
-        # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
-        # END_YOUR_CODE
+        index, _ = state 
+        return index == len(self.query)
 
     def succAndCost(self, state):
-        # BEGIN_YOUR_CODE (our solution is 14 lines of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
-        # END_YOUR_CODE
+        successors = []
+        
+        index, previous_word = state 
+        
+        for end_index in range(index + 1, len(self.query) + 1):
+            current_word = self.query[index:end_index]
+            possible_words = self.possibleFills(current_word)
+            
+            if possible_words is None:
+                continue 
+            
+            for word in possible_words:
+                successors.append((word, (end_index, word), self.bigramCost(previous_word, word)))
+        
+        return successors
 
 
 def segmentAndInsert(query: str, bigramCost: Callable[[str, str], float],
@@ -106,9 +128,12 @@ def segmentAndInsert(query: str, bigramCost: Callable[[str, str], float],
     if len(query) == 0:
         return ''
 
-    # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+    ucs = util.UniformCostSearch(verbose=0)
+    ucs.solve(JointSegmentationInsertionProblem(query, bigramCost, possibleFills))
+
+    if ucs.actions is None:
+        return '' # cannot segment this query
+    return ' '.join(ucs.actions) # minimum-cost path is the segmented words with lowest bigram cost
 
 
 ############################################################
